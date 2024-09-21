7c9b5ca23585feddbb10730c1dcae40a7141f0f4
GodMakesMe <76955563+GodMakesMe@users.noreply.github.com>
2024-09-21 17:56:23 +0530

    Add files via upload

---
+++ core/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Vector;
+
+int falldamage;
+class enemy{
+	int health;
+	int selfdamage;
+}
+class bird{
+	int damage;
+}
+class block{
+	int health;
+	int damage;
+}
+class gui{
+	
+}
+
+
+public class Main {
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Our College Project of Angry Birds...");
+	}
+}
