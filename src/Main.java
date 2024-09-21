aef8de2cce5756974044dde87ff685370c9a9d09
GodMakesMe <jaypeekrishna17@gmail.com>
2024-09-21 15:49:47 +0530

    commiting

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,23 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Vector;
 
+int falldamage;
+class enemy{
+	int health;
+	int selfdamage;
+}
+class bird{
+	int damage;
+}
+class block{
+	int health;
+	int damage;
+}
+
 
 public class Main {
 	public static void main(String[] args) {
+
 		System.out.println("Welcome to Our College Project of Angry Birds...");
 	}
 }
