165af1972c56d75f5415e0ade54af318ee01cd02
GodMakesMe <jaypeekrishna17@gmail.com>
2024-10-13 19:51:04 +0530

    commiting

--- core/src/main/java/com/unreal/angrybirds/Main.java
+++ core/src/main/java/com/unreal/angrybirds/Main.java
@@ -4,8 +4,11 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
 
@@ -14,17 +17,27 @@ import java.awt.*;
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
+    private Stage stage;
+    private Sprite sprite;
     private Texture image;
-    private Texture buttonImage;
-    private Button imageButton;
+    private TextureRegion buttonImage;
+    private ImageButton imageButton;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
         image = new Texture("HomePage.png"); // Change it to libgdx.png
-        buttonImage = new Texture("StartButton.png");
-        imageButton = new Button("Start Game");
-        imageButton.setSize(buttonImage.getWidth(), buttonImage.getHeight());
+        batch = new SpriteBatch();
+        sprite = new Sprite(image);
+        sprite.setSize(image.getWidth()/4, image.getHeight()/4);
+        stage = new Stage();
+        buttonImage = new TextureRegion(new Texture("StartButton.png"));
+        imageButton = new ImageButton(new TextureRegionDrawable(buttonImage));
+        imageButton.setSize(100, 100);
+        imageButton.setSize(buttonImage.getRegionWidth(), buttonImage.getRegionHeight());
+        imageButton.setPosition(Gdx.graphics.getWidth() / 2 - imageButton.getWidth() / 2,Gdx.graphics.getHeight() / 2 - imageButton.getHeight() / 2);
+        stage.addActor(imageButton);
+        Gdx.input.setInputProcessor(stage);
+//        imageButton.setSize(buttonImage.getWidth(), buttonImage.getHeight());
 //        imageButton.createImage()
     }
 
@@ -32,9 +45,15 @@ public class Main extends ApplicationAdapter {
     public void render() {
 //        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         ScreenUtils.clear(0.10f, 0.10f, 0.1f, 1f);
+//        sprite.setPosition(0, 0);
+//        sprite.setSize(5120/4, 2880/4);
+//        sprite.setOrigin(sprite.getWidth() / 2, sprite.getHeight() / 2);
         batch.begin();
-        batch.draw(image, 0, 0, 5120/4, 2880/4);
+        sprite.draw(batch);
         batch.end();
+
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
     }
 
     @Override
