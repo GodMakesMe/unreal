86e424319c79821c75bd38701fe3194ec0a3ef25
Malav Solanki <69003751+MalavSolanki18@users.noreply.github.com>
2024-10-15 02:49:52 +0530

    fixing the logic of Game, HomePage and SeasonPage

--- core/src/main/java/com/unreal/angrybirds/Main.java
+++ core/src/main/java/com/unreal/angrybirds/Main.java
@@ -1,64 +1,18 @@
 package com.unreal.angrybirds;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-import java.awt.*;
-
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Stage stage;
-    private Sprite sprite;
-    private Texture image;
-    private TextureRegion buttonImage;
-    private ImageButton imageButton;
-
+public class Main extends Game {
     @Override
     public void create() {
-        image = new Texture("HomePage.png"); // Change it to libgdx.png
-        batch = new SpriteBatch();
-        sprite = new Sprite(image);
-        sprite.setSize(image.getWidth()/4, image.getHeight()/4);
-        stage = new Stage();
-        buttonImage = new TextureRegion(new Texture("StartButton.png"));
-        imageButton = new ImageButton(new TextureRegionDrawable(buttonImage));
-        imageButton.setSize(100, 100);
-        imageButton.setSize(buttonImage.getRegionWidth(), buttonImage.getRegionHeight());
-        imageButton.setPosition(Gdx.graphics.getWidth() / 2 - imageButton.getWidth() / 2,Gdx.graphics.getHeight() / 2 - imageButton.getHeight() / 2);
-        stage.addActor(imageButton);
-        Gdx.input.setInputProcessor(stage);
-//        imageButton.setSize(buttonImage.getWidth(), buttonImage.getHeight());
-//        imageButton.createImage()
+        setScreen(new HomePage(this));
     }
 
-    @Override
-    public void render() {
-//        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        ScreenUtils.clear(0.10f, 0.10f, 0.1f, 1f);
-//        sprite.setPosition(0, 0);
-//        sprite.setSize(5120/4, 2880/4);
-//        sprite.setOrigin(sprite.getWidth() / 2, sprite.getHeight() / 2);
-        batch.begin();
-        sprite.draw(batch);
-        batch.end();
-
-        stage.act(Gdx.graphics.getDeltaTime());
-        stage.draw();
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
-    }
 }
---
+++ core/src/main/java/com/unreal/angrybirds/SeasonPage.java
@@ -0,0 +1,98 @@
+package com.unreal.angrybirds;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class SeasonPage implements Screen {
+    private Main Game;
+    private OrthographicCamera camera;
+    private Stage stage;
+    private SpriteBatch batch;
+    private Sprite sprite;
+    private ImageButton Startbutton;
+
+    public SeasonPage(Main game) {
+        this.Game = game;
+    }
+    @Override
+    public void show() {
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.position.set(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, 0); // Set camera position to center
+        camera.update();
+        sprite = new Sprite(new Texture("assets/SeasonPage.png"));
+        batch = new SpriteBatch();
+
+        sprite.setPosition(0, 0);
+        sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+//        Startbutton = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture("assets/data/start.png"))));
+//        Startbutton.setPosition(522,720-466-72);
+//        stage.addActor(Startbutton);
+
+//        Startbutton.addListener(new InputListener(){
+//            @Override
+//            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+//                System.out.println("Button is clicked!!!!!!");
+//                System.out.println("Button position: " + Startbutton.getX() + ", " + Startbutton.getY());
+//                return true;
+//            }
+//        });
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        ScreenUtils.clear(1, 1, 1, 1);
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        sprite.draw(batch);
+        batch.end();
+
+//        stage.act(Gdx.graphics.getDeltaTime());
+//        stage.setDebugAll(true);
+//        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        stage.dispose();
+        sprite.getTexture().dispose();
+    }
+}
