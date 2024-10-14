362a835d0a33c2b4ff3318bad747f8fd088df4ab
Malav Solanki <69003751+MalavSolanki18@users.noreply.github.com>
2024-10-15 03:42:36 +0530

    fixing the logic of Game, HomePage and SeasonPage

--- core/src/main/java/com/unreal/angrybirds/HomePage.java
+++ core/src/main/java/com/unreal/angrybirds/HomePage.java
@@ -1,24 +1,21 @@
 package com.unreal.angrybirds;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class HomePage implements Screen {
     public Main Game;
@@ -27,6 +24,8 @@ public class HomePage implements Screen {
     private SpriteBatch batch;
     private Sprite sprite;
     private ImageButton Startbutton;
+    private Texture Start;
+    private Texture HoverStart;
 
     public HomePage(Main game) {
         this.Game = game;
@@ -43,9 +42,16 @@ public class HomePage implements Screen {
 
         sprite.setPosition(0, 0);
         sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Start = new Texture("assets/start.png");
+        HoverStart = new Texture("assets/hoverStart.png");
 
-        Startbutton = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture("assets/start.png"))));
+        ImageButton.ImageButtonStyle style = new ImageButton.ImageButtonStyle();
+        style.up = new TextureRegionDrawable(new TextureRegion(Start));
+        style.over = new TextureRegionDrawable(new TextureRegion(HoverStart));
+
+        Startbutton = new ImageButton(style);
         Startbutton.setPosition(522,720-466-72);
+        Startbutton.setSize(202,72);
         stage.addActor(Startbutton);
 
         Startbutton.addListener(new InputListener(){
@@ -69,7 +75,6 @@ public class HomePage implements Screen {
         batch.end();
 
         stage.act(Gdx.graphics.getDeltaTime());
-        stage.setDebugAll(true);
         stage.draw();
     }
 
@@ -98,5 +103,7 @@ public class HomePage implements Screen {
         batch.dispose();
         stage.dispose();
         sprite.getTexture().dispose();
+        Start.dispose();
+        HoverStart.dispose();
     }
 }
