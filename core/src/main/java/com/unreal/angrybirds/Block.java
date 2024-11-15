48a6a14d0c215ce9b414c393927c7777d2be0fe9
GodMakesMe <jaypeekrishna17@gmail.com>
2024-11-16 01:38:34 +0530

    Initialization Of Block

--- core/src/main/java/com/unreal/angrybirds/Block.java
+++ core/src/main/java/com/unreal/angrybirds/Block.java
@@ -1,20 +1,44 @@
 package com.unreal.angrybirds;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.World;
 
-public class Block {
-    World worldInstance;
+import java.io.Serializable;
+
+public class Block implements Serializable {
+    transient World worldInstance;
     int height;
     int width;
     int saveScalex;
     int saveScaley;
     int mass;
-    Texture blockTexture;
+    transient Texture blockTexture;
+    transient Sprite blockSprite;
+
+    private transient Body BirdBody;
+    private transient BodyDef BirdBodydef;
 
     Block(String imageFile, int scalex, int scaley, World world, int mass) {
         blockTexture = new Texture(imageFile);
+        blockSprite = new Sprite(blockTexture);
+        blockSprite.setSize(scalex, scaley);
+        this.mass = mass;
+        this.worldInstance = world;
+        this.height = scalex;
+        this.width = scaley;
+        this.saveScalex = scalex;
+        this.saveScaley = scaley;
 
+    }
+
+    public Block() {
+    }
+
+    void placeBlock(int x, int y) {
+        blockSprite.setPosition(x, y);
     }
 }
 
